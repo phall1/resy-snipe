a0737a986789029df6e2e344454e12229ca25224
Speed Racer <drspeedracer@Speeds-iMac.fios-router.home>
2023-07-22 10:15:42 -0400

    updating date info

--- config/resy-config.go
+++ config/resy-config.go
@@ -26,33 +26,31 @@ func NewReservationTimeType(reservationTime string, tableType *string) Reservati
     return ReservationTimeType{ReservationTime: reservationTime, TableType: tableType}
 }
 
+RESY_API_KEY
+Define ENV vars for RESY Credentials
+// var ResyKeyss = ResyKeys{ApiKey: os.Getenv("RESY_API_KEY"), AuthToken: os.Getenv("RESY_API_KEY")}
 var ResyKeyss = ResyKeys{ApiKey: os.Getenv("RESY_API_KEY"), AuthToken: os.Getenv("RESY_API_KEY")}
 var SnipeTimee = SnipeTime{Hours: 0, Minutes: 0}
 // var tableType = "Dining Room"
 // var tableType = "Taproom Table"
 var ResTimeTypes = []ReservationTimeType{
     NewReservationTimeType("19:00:00", nil),
-    NewReservationTimeType("19:15:00", nil),
     NewReservationTimeType("18:45:00", nil),
+    NewReservationTimeType("19:15:00", nil),
     NewReservationTimeType("18:30:00", nil),
     NewReservationTimeType("18:15:00", nil),
     NewReservationTimeType("18:00:00", nil),
     NewReservationTimeType("19:45:00", nil),
-    // NewReservationTimeType("12:15:00", &tableType),
-    // NewReservationTimeType("16:00:00", nil),
-    // NewReservationTimeType("12:30:00", nil),
-    // NewReservationTimeType("19:15:00", nil),
-    // NewReservationTimeType("19:30:00", nil),
-    // NewReservationTimeType("18:30:00", nil),
-    // NewReservationTimeType("18:30:00", &tableType),
+    NewReservationTimeType("17:45:00", nil),
 }
 
 // DeadRabbit: 38660
 // Rubirosa: 466
 // Red Pearl: 69820
+// Raf's: 65679
 var ReservationDetailss = ReservationDetails{
-    Date:         "2023-06-30",
-    PartySize:    2,
+    Date:         "2023-07-22",
+    PartySize:    4,
     VenueId:      466,
     ResTimeTypes: ResTimeTypes,
 }
